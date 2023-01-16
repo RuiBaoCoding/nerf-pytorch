9e0cc6a9735d50d86bda2d1645910e1ff9f4ed0e
Serendipity <104818872+RuiBaoCoding@users.noreply.github.com>
2023-01-16 17:14:38 +0800

    添加注释

--- load_blender.py
+++ load_blender.py
@@ -33,11 +33,22 @@ def pose_spherical(theta, phi, radius):
     c2w = torch.Tensor(np.array([[-1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]])) @ c2w
     return c2w
 
-
+'''
+获取图片、位姿、图片的长宽与焦距、划分训练集测试集
+RGBA，A是指透明度
+'''
 def load_blender_data(basedir, half_res=False, testskip=1):
+    '''
+    basedir:数据集文件夹
+    half_res:是否对图像进行半裁剪
+    testskip:挑选测试数据集的跳跃步长
+    '''
     splits = ['train', 'val', 'test']
+    #是个字典
     metas = {}
     for s in splits:
+        #fp：filepath，根据三个后缀分别找到相应的json文件
+        #basedir：./data/nerf_syntheticc/lego
         with open(os.path.join(basedir, 'transforms_{}.json'.format(s)), 'r') as fp:
             metas[s] = json.load(fp)
 
@@ -45,6 +56,7 @@ def load_blender_data(basedir, half_res=False, testskip=1):
     all_poses = []
     counts = [0]
     for s in splits:
+        #meta中存的是相应json的数据，也是字典
         meta = metas[s]
         imgs = []
         poses = []
@@ -52,7 +64,8 @@ def load_blender_data(basedir, half_res=False, testskip=1):
             skip = 1
         else:
             skip = testskip
-            
+        
+        #以指定步长读取内容
         for frame in meta['frames'][::skip]:
             fname = os.path.join(basedir, frame['file_path'] + '.png')
             imgs.append(imageio.imread(fname))
@@ -63,15 +76,19 @@ def load_blender_data(basedir, half_res=False, testskip=1):
         all_imgs.append(imgs)
         all_poses.append(poses)
     
+    #给split标序号
     i_split = [np.arange(counts[i], counts[i+1]) for i in range(3)]
-    
+    #（138,800,800,4）
     imgs = np.concatenate(all_imgs, 0)
+    #（138,4,4）外参
     poses = np.concatenate(all_poses, 0)
     
     H, W = imgs[0].shape[:2]
     camera_angle_x = float(meta['camera_angle_x'])
+    #算焦距
     focal = .5 * W / np.tan(.5 * camera_angle_x)
     
+    #40个4*4，渲染的pose
     render_poses = torch.stack([pose_spherical(angle, -30.0, 4.0) for angle in np.linspace(-180,180,40+1)[:-1]], 0)
     
     if half_res:
@@ -82,6 +99,7 @@ def load_blender_data(basedir, half_res=False, testskip=1):
         imgs_half_res = np.zeros((imgs.shape[0], H, W, 4))
         for i, img in enumerate(imgs):
             imgs_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
+        #（138,400,400,4）138是个数
         imgs = imgs_half_res
         # imgs = tf.image.resize_area(imgs, [400, 400]).numpy()
 
